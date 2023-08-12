abb84ee965dbe3c2d1ddbefb4e2528498baca352
Andrew Westgerdes <ajwestgerdes@gmail.com>
2023-08-12 02:17:22 -0500

    all function data now gets exported to excel sheet. Still not iterating through every function for some reason

--- create_excel.py
+++ create_excel.py
@@ -1,30 +1,20 @@
 import pandas
 
+grouped_data = {}
+
+# df1 = pd.DataFrame([['a', 'b'], ['c', 'd']],
+#                    index=['row 1', 'row 2'],
+#                    columns=['col 1', 'col 2'])
+# df1.to_excel("output.xlsx")  
+
 def create_excel(data):
-# create an empty dataframe
-df = pandas.DataFrame()
-
-# loop through your flat dictionary and add each key-value pair to the dataframe
-for key, value in flat_dict.items():
-    df[key] = value
-
-# create a new Excel writer object
-writer = pandas.ExcelWriter('output.xlsx', engine='xlsxwriter')
-
-# write the dataframe to a new sheet in the Excel file
-df.to_excel(writer, sheet_name='Sheet1', index=False)
-
-# continue adding to the same sheet over time
-for i in range(10):
-    # create a new dataframe with new data
-    new_df = pandas.DataFrame({'new_key': [i], 'new_value': [f'new_value_{i}']})
-
-    # add the new data to the existing dataframe
-    df = pandas.concat([df, new_df], axis=0)
-
-    # write the updated dataframe to the same sheet in the Excel file
-    df.to_excel(writer, sheet_name='Sheet1', index=False)
-
-# save and close the Excel writer
-writer.save()
-writer.close()
+    print(data)
+    df = pandas.DataFrame({
+        "Function Name": data['function_name'],
+        "Last Invocation (days)": data['last_invocation'],
+        "Last Lambda Modification": data['last_modified'],
+        "Code Size (bytes)": data['code_size'],
+        "Runtime": data['runtime'],
+        "Description": data['description']
+    })
+    df.to_excel('test.xlsx', sheet_name='Lambda Report', index=False)
--- lambda_reporter.py
+++ lambda_reporter.py
@@ -1,48 +1,56 @@
 import boto3
 import datetime
+from create_excel import create_excel
 
-client = boto3.client('lambda')
+lambda_client = boto3.client('lambda')
+logs_client = boto3.client('logs')
 
-def handler(event, context):
+function_name_list = []
+last_invocation_list = []
+last_modified_list = []
+code_size_list = []
+runtime_list = []
+description_list = []
+
+def handler():
     #Iterate through every lambda function we have
-    response = client.list_functions()
+    response = lambda_client.list_functions()
     for function in response['Functions']:
         function_name = function['FunctionName']
-        #Find the last date for each cloudwatch log and get days since
         last_log_date = get_last_log_date(function_name)
-        print(last_log_date)
-        if last_log_date:
-            print(f"{function_name}: {last_log_date}")
-        else:
-            print(f"{function_name}: No logs found")
+        config = get_function_configuration(function_name)
+        function_name_list.append(function_name)
+        last_invocation_list.append(last_log_date)
+        last_modified_list.append(config["LastModified"].split("T")[0])
+        code_size_list.append(config["CodeSize"])
+        runtime_list.append(config["Runtime"])
+        description_list.append(config["Description"])
+    create_excel({
+        "function_name": function_name_list,
+        "last_invocation": last_invocation_list,
+        "last_modified": last_modified_list,
+        "code_size": code_size_list,
+        "runtime": runtime_list,
+        "description": description_list
+    })
+
 
 def get_last_log_date(function_name):
-    response = client.list_functions()
-    functions = [f for f in response['Functions'] if f['FunctionName'] == function_name]
-    if functions:
-        function_arn = functions[0]['FunctionArn']
-        response = client.list_tags(Resource=function_arn)
-        log_group_name = [t['Value'] for t in response['Tags'] if t['Key'] == 'LogGroup'][0]
-        response = client.filter_log_events(
-            logGroupName=log_group_name,
-            startTime=int((datetime.datetime.now() - datetime.timedelta(days=7)).timestamp() * 1000),
-            endTime=int(datetime.datetime.now().timestamp() * 1000),
-            filterPattern='""',
-            limit=1
-        )
-        if response['Events']:
-            last_event = response['Events'][0]
-            last_event_time = datetime.datetime.fromtimestamp(last_event['Timestamp'] / 1000)
-            return last_event_time.date()
-    return None
+    response = logs_client.describe_log_groups(logGroupNamePrefix='/aws/lambda/' + function_name)
+    log_group = response['logGroups']
+    if log_group:
+        timestamp_datetime = datetime.datetime.fromtimestamp(log_group[0]['creationTime'] / 1000)
+        now = datetime.datetime.now()
+        time_difference = now - timestamp_datetime
+        return int(time_difference.total_seconds() / (60 * 60 * 24))
+    else:
+        return None
 
 #Get all other lambda configuration stats
 def get_function_configuration(function_name):
-    response = client.get_function(FunctionName=function_name)
+    response = lambda_client.get_function(FunctionName=function_name)
     if response:
         return response['Configuration']
     return None
 
-    #Put all data into a list of dicts and dump to excel file
-
-    #Upload excel file to s3
+handler()
